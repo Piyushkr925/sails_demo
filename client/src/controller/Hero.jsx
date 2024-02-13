7361db0d641dcbded0db398d093dc9f86beeab82
Piyushkr925 <piyushkr925@gmail.com>
2024-02-13 18:00:04 +0530

    committ

--- client/src/controller/Hero.jsx
+++ client/src/controller/Hero.jsx
@@ -4,7 +4,7 @@ const Hero = () => {
   return (
     <div className='text-white'>
       <div className='max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center'>
-        <p className='text-[#00df9a] font-bold p-2'>GROWING WITH DATA ANALYTICS</p>
+        <p className='text-[#00df9a] font-bold p-2'>GROWINGG WITH DATA ANALYTICS</p>
       </div>
       
     </div>
