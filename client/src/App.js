52536f87685b924012c515ec77381d37f747ba8e
Piyushkr925 <piyushkr925@gmail.com>
2024-02-14 12:34:45 +0530

    Second Commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Hero from './controller/Hero';
 import Navbar from './controller/Navbar';
+import Analytics from './controller/Analytics';
+import NewsLetter from './controller/NewsLetter';
 
 function App() {
   return (
     <>
     <Navbar/>
     <Hero/>
+    <Analytics />
+    <NewsLetter />
     </>
   );
 }
