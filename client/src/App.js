b80b2fd82d2671569551b4971d1cbdfa4a34cc7f
Piyushkr925 <piyushkr925@gmail.com>
2024-02-14 19:13:10 +0530

    Forth

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import Hero from './controller/Hero';
 import Navbar from './controller/Navbar';
 import Analytics from './controller/Analytics';
 import NewsLetter from './controller/NewsLetter';
+import Cards from './controller/Cards';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
     <Hero/>
     <Analytics />
     <NewsLetter />
+    <Cards />
     </>
   );
 }
